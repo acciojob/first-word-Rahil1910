d04c452b608965de2e4b45f19c67cc6076fe7012
Rahil1910 <rahilrafique1234@gmail.com>
2024-11-30 12:49:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function firstWord(s) {
   // your code here
+	let cntW=0;
+	let ans="";
+	for(let i=0;i<s.length;i++){
+		if(s[i]==" "&&cntW==0){
+			continue;
+		}
+		cntW++;
+		ans+=s[i];
+		if(s[i]==" "&&cntW>0){
+			return ans;
+		}
+	}
 }
-
+ 
 // Do not change the code below
 
 const s = prompt("Enter String:");
